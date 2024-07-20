840c99b30166dfdbdaa978867f5715ee097644df
NidhiPednekar <146639573+NidhiPednekar@users.noreply.github.com>
2024-07-20 16:32:11 +0530

    Created Meme.jsx

---
+++ Meme.jsx
@@ -0,0 +1,11 @@
+export default function Meme(){
+  return (
+    <div>
+      <form action="" className="container">
+        <input type="text"  placeholder="Shut up"/>
+        <input type="text" placeholder="and take my money" />
+        <button className="genMeme">Get a new Meme 🏞️</button>
+      </form>
+    </div>
+  )
+}
