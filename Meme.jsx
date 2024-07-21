70c992aedf62ec7e6fe40dde720c37aaaba94340
NidhiPednekar <146639573+NidhiPednekar@users.noreply.github.com>
2024-07-22 00:34:36 +0530

    Updated Meme.jsx

--- Meme.jsx
+++ Meme.jsx
@@ -1,11 +1,27 @@
+import memesData from "./memesData"
+import React from "react"
+
 export default function Meme(){
+
+  const [memeImage,setMemeImage] = React.useState('')
+
+  function getImage(){
+    const memesArr = memesData.data.memes
+    const randomNo = Math.floor(Math.random() * memesArr.length)
+    const url = memesArr[randomNo].url
+    console.log(url)
+    setMemeImage(url)
+    
+
+  }
   return (
     <div>
-      <form action="" className="container">
+      <form action="" className="container" id="form">
         <input type="text"  placeholder="Shut up"/>
         <input type="text" placeholder="and take my money" />
-        <button className="genMeme">Get a new Meme 🏞️</button>
+        <button onClick={getImage} className="genMeme" type="button">Get a new Meme 🏞️</button>
       </form>
+      <img src={memeImage} alt="" className="meme-image"/>
     </div>
   )
 }
