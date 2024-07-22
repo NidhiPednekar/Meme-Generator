c35bdb7b80af67f67c9fd075b32f9d4e39ed7c51
NidhiPednekar <146639573+NidhiPednekar@users.noreply.github.com>
2024-07-22 10:11:35 +0530

    uploaded memesData.jsx

---
+++ memesData.jsx
@@ -0,0 +1,168 @@
+export default{
+  "success":true,
+  "data":{
+    "memes": [
+      {
+        "id": "1",
+        "name": "Drake Hotline Bling",
+        "url": "https://i.imgflip.com/30blgx.jpg",
+        "width": 1200,
+        "height": 1200,
+        "box_count": 2
+      },
+      {
+        "id": "2",
+        "name": "Two Buttons",
+        "url": "https://i.imgflip.com/1g8my4.jpg",
+        "width": 600,
+        "height": 908,
+        "box_count": 3
+      },
+      {
+        "id": "3",
+        "name": "Distracted Boyfriend",
+        "url": "https://i.imgflip.com/1ur9b0.jpg",
+        "width": 1200,
+        "height": 800,
+        "box_count": 3
+      },
+      {
+        "id": "4",
+        "name": "Change My Mind",
+        "url": "https://i.imgflip.com/24y43o.jpg",
+        "width": 482,
+        "height": 361,
+        "box_count": 2
+      },
+      {
+        "id": "5",
+        "name": "Left Exit 12 Off Ramp",
+        "url": "https://i.imgflip.com/22bdq6.jpg",
+        "width": 804,
+        "height": 767,
+        "box_count": 2
+      },
+      {
+        "id": "6",
+        "name": "Gru's Plan",
+        "url": "https://i.imgflip.com/26jxvz.jpg",
+        "width": 700,
+        "height": 449,
+        "box_count": 4
+      },
+      {
+        "id": "7",
+        "name": "Expanding Brain",
+        "url": "https://i.imgflip.com/1jwhww.jpg",
+        "width": 857,
+        "height": 1202,
+        "box_count": 4
+      },
+      {
+        "id": "8",
+        "name": "Running Away Balloon",
+        "url": "https://i.imgflip.com/261o3j.jpg",
+        "width": 761,
+        "height": 1024,
+        "box_count": 4
+      },
+      {
+        "id": "9",
+        "name": "Mocking Spongebob",
+        "url": "https://i.imgflip.com/1otk96.jpg",
+        "width": 502,
+        "height": 353,
+        "box_count": 2
+      },
+      {
+        "id": "10",
+        "name": "Boardroom Meeting Suggestion",
+        "url": "https://i.imgflip.com/m78d8.jpg",
+        "width": 500,
+        "height": 649,
+        "box_count": 4
+      },
+      {
+        "id": "11",
+        "name": "One Does Not Simply",
+        "url": "https://i.imgflip.com/1bij.jpg",
+        "width": 568,
+        "height": 335,
+        "box_count": 2
+      },
+      {
+        "id": "12",
+        "name": "Leonardo DiCaprio Cheers",
+        "url": "https://i.imgflip.com/39t1o.jpg",
+        "width": 600,
+        "height": 400,
+        "box_count": 2
+      },
+      {
+        "id": "13",
+        "name": "Brace Yourself",
+        "url": "https://i.imgflip.com/1h7in3.jpg",
+        "width": 622,
+        "height": 477,
+        "box_count": 2
+      },
+      {
+        "id": "14",
+        "name": "X, X Everywhere",
+        "url": "https://i.imgflip.com/1ihzfe.jpg",
+        "width": 2118,
+        "height": 1440,
+        "box_count": 2
+      },
+      {
+        "id": "15",
+        "name": "Futurama Fry",
+        "url": "https://i.imgflip.com/1bgw.jpg",
+        "width": 552,
+        "height": 414,
+        "box_count": 2
+      },
+      {
+        "id": "16",
+        "name": "Batman Slapping Robin",
+        "url": "https://i.imgflip.com/9ehk.jpg",
+        "width": 400,
+        "height": 387,
+        "box_count": 2
+      },
+      {
+        "id": "17",
+        "name": "Confession Bear",
+        "url": "https://i.imgflip.com/1al46.jpg",
+        "width": 500,
+        "height": 500,
+        "box_count": 2
+      },
+      {
+        "id": "18",
+        "name": "Success Kid",
+        "url": "https://i.imgflip.com/1bhk.jpg",
+        "width": 500,
+        "height": 500,
+        "box_count": 2
+      },
+      {
+        "id": "19",
+        "name": "Ancient Aliens",
+        "url": "https://i.imgflip.com/1zy9.jpg",
+        "width": 500,
+        "height": 437,
+        "box_count": 2
+      },
+      {
+        "id": "20",
+        "name": "First World Problems",
+        "url": "https://i.imgflip.com/1bhf.jpg",
+        "width": 500,
+        "height": 500,
+        "box_count": 2
+      }
+      
+    ]
+  }
+}
