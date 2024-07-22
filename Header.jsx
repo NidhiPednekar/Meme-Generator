5f7b599fae9fef9057376a3e7fcc231d92eb15c3
NidhiPednekar <146639573+NidhiPednekar@users.noreply.github.com>
2024-07-22 17:58:51 +0530

    Updated Header.jsx

--- Header.jsx
+++ Header.jsx
@@ -1,17 +1,14 @@
 import meme from '../public/meme.jpeg'
 
+
 export default function Header(){
+ 
   return(
     <header>
-      <img src={meme} alt="" /> <h2 className='header-title'>Meme Generator </h2>
-      <h4>Make your day with Memes!!</h4>
+      <img  src={meme} alt="" /> <h2 className='header-title'>Meme Generator </h2>
+      <h4>Memes to Light Up Your Mood!</h4>
     </header>
   )
 }
-
-
-
-
-
 
 
